971c9bf9f0d8fe9c1e21607b58689568b0375d52
MetalFatigue <rafal.kiwadowicz@gmail.com>
2022-07-27 22:55:02 +0200

    #2 Strona główna-Dynamiczne ładowanie instytucji

--- share/views.py
+++ share/views.py
@@ -8,9 +8,18 @@ class LandingPage(View):
     def get(self, request):
         bags = Donation.objects.all().aggregate(Sum('quantity'))
         institutions = Institution.objects.all()
+        foundations = institutions.filter(type=0)
+        non_government_organization = institutions.filter(type=1)
+        community_collection = institutions.filter(type=2)
         donated_institutions = institutions.filter(donation__quantity__gt=0).count()
-        return render(request, "index.html", {"bags": bags, "institutions": institutions,
-                                              "donated_institution": donated_institutions})
+        context = {
+            "bags": bags,
+            "foundations": foundations,
+            "non_government_organization": non_government_organization,
+            "community_collection": community_collection,
+            "donated_institution": donated_institutions
+        }
+        return render(request, "index.html", context)
 
 
 class AddDonation(View):
